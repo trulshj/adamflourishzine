9c3da07317d063e03998043102b96f07928526ec
Truls Henrik Jakobsen <trulshj@stud.ntnu.no>
2021-10-24 23:14:39 +0200

    remove unused imports

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,3 @@
-import Head from 'next/head';
-import Image from 'next/image';
 import Link from 'next/link';
 import utilStyles from '../styles/utils.module.css';
 import layoutStyles from '../components/layout.module.css';
